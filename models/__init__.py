0616dad5dcf216016f85fe4228801387719fea08
AmorosoLaura <amoroso.laura22@gmail.com>
2024-06-07 18:16:26 +0200

    renaming files models

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,8 @@
 from models.VideoModel import VideoModel
 from models.I3D import I3D
-from models.FinalClassifier import MLP_classifier, MLP_aggregation_classifier
-from models.Lstm_classifier import Lstm_classifier
-from models.Transformer_classifier import Transformer_classifier
-from models.TRN_classifier import TRN_classifier
-from models.emg_lstm import EMG_LSTM
-from models.emg_lenet import LeNet5
+from models.MLP_rgb import MLP_classifier, MLP_aggregation_classifier
+from models.LSTM_rgb import Lstm_classifier
+from models.TRN_rgb import TRN_classifier
+from models.LSTM_emg import EMG_LSTM
+from models.CNN_emg import LeNet5
 from models.MidLevelFC_classifier import FullyConnectedFusion
